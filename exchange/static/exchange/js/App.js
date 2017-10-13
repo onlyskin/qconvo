e308cce1353f4f49f2d3f0aec4d1e55c54e1791e
Sam Szreter <s.szreter@gmail.com>
2017-10-13 17:01:31 +0100

    Add mithril routing to frontend for profile page

--- exchange/static/exchange/js/App.js
+++ exchange/static/exchange/js/App.js
@@ -5,11 +5,29 @@ var ctrl = new Ctrl(model, dataPopulator);
 
 dataPopulator.initialise();
 
-var App = {
+var Header = {
+    view: function() {
+        return m('#header', [
+            m("a[href=/search]", {oncreate: m.route.link}, 'search'),
+            m("a[href=/profile]", {oncreate: m.route.link}, 'profile'),
+        ]);
+    }
+}
+
+var Search = {
     view: function () {
         return m('',
+            m(Header),
             inputsView(),
             resultsView(model.users)
+        );
+    }
+}
+
+var Profile = {
+    view: function() {
+        return m('',
+            m(Header),
         );
     }
 }
